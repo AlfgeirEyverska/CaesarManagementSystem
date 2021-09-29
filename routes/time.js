1dd46a14e1250868d09d088342e6080a03bd6728
TK <tyler.korte@maxar.com>
2021-09-28 20:16:56 -0400

    Initial commit

---
+++ routes/time.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const date = require('date-and-time');
+
+function update_clock(req, res, next, io) {
+    setTimeout(() => {
+        const now = new Date();
+        let update = date.format(now, 'hh:mm:ss A');
+        io.emit('tick', update);
+        console.log("Clock Updated!");
+        update_clock(req, res, next, io)
+    }, 1000);
+}
+
+/* GET time listing. */
+router.get('/', function(req, res, next) {
+    const io = req.app.get('socketio');
+    res.sendFile(path.join(__dirname, '../public/time.html'));
+
+    update_clock(req, res, next, io);
+});
+
+module.exports = router;
